74790b8fb46d48addd79fe453ec940f04998dc3b
AndreasKaer <42870551+AndreasKaer@users.noreply.github.com>
2018-09-10 23:22:00 +0200

    Add files via upload

---
+++ Opg_A3/src/opg_a3/Opg_A3.java
@@ -0,0 +1,28 @@
+
+package opg_a3;
+
+public class Opg_A3 {
+
+    public static void main(String[] args) {
+        
+        // Define my temperatures 
+        double temp1 = 21.5;
+        double temp2 = 23.7;
+        double temp3 = 19.6;
+        double temp4 = 22.5;
+        double temp5 = 25.3;
+        double temp6 = 21.7;
+        double temp7 = 18.9;
+        
+        // Calculates and computes the temperature difference between the given dates
+        System.out.println("The temperature differnce between monday and thuesday is " + (temp2 - temp1);
+        System.out.println("The temperature differnce between thuesday and wednesday is " + (temp3 - temp2);
+        System.out.println("The temperature differnce between wednesday and thursday is " + (temp4 - temp3);
+        System.out.println("The temperature differnce between thursday and friday is " + (temp5 - temp4);
+        System.out.println("The temperature differnce between friday and saturday is " + (temp6 - temp5);
+        System.out.println("The temperature differnce between saturday and sunday is " + (temp7 - temp6);
+        
+       
+    }
+    
+}
