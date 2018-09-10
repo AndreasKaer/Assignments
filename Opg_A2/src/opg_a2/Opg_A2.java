3c027c14d252be8afabd1c85fd0d67911c728ae7
AndreasKaer <42870551+AndreasKaer@users.noreply.github.com>
2018-09-10 23:20:51 +0200

    Add files via upload

---
+++ Opg_A2/src/opg_a2/Opg_A2.java
@@ -0,0 +1,21 @@
+
+package opg_a2;
+
+public class Opg_A2 {
+
+    public static void main(String[] args) {
+        
+        // Defien the radius of the two circles 
+        int rad1 = 3;
+        int rad2 = 7;
+        
+        int sum = (int) (2 * 3.14 * (rad1 + rad2));
+        
+        System.out.println("The sum of the circumference of the two circles are " + sum);
+       
+        
+        
+     
+    }
+    
+}
