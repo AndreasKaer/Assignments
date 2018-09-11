b59c694ca61af0dabeb89ae7d418e97d978dce60
AndreasKaer <42870551+AndreasKaer@users.noreply.github.com>
2018-09-11 23:53:48 +0200

    Add files via upload

---
+++ Liang_2_7B/src/liang_2_7b/Liang_2_7B.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package liang_2_7b;
+
+/**
+ *
+ * @author akl49
+ */
+public class Liang_2_7B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
